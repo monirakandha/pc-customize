8c6c57f916ef609d5309735fe7e9fc80de4bae43
monirhosenakanda <monirhosenakanda@gmail.com>
2022-02-15 12:50:07 +0600

    coupon code added

--- js/script.js
+++ js/script.js
@@ -15,12 +15,14 @@ function updatePrice (iteamId , price){
 
 };
 
+//onlcick function 
 function onClick (clickId , updateId , price){
     document.getElementById(clickId).addEventListener("click",function(){
         updatePrice(updateId , price);
-        });
+        document.getElementById(clickId).style.backgroundColor = "cornflowerblue"
+});
 }
-
+//ADD all id with parameter
 onClick("eightGB", "memory-cost" , 200 );
 onClick("sixteenGB" , "memory-cost" , 300);
 onClick("ssd1" , "storage-cost" , 700);
@@ -28,3 +30,23 @@ onClick ("ssd2" , "storage-cost" , 1300);
 onClick ("ssd3" , "storage-cost" , 2500);
 onClick("free-delivery" , "delivery-cost" , 0);
 onClick("paid-delivery" , "delivery-cost" , 40);
+
+
+//coupon code 
+const couponCode = "pHero";
+document.getElementById("apply-btn").addEventListener('click', function(){
+    const promoInput = document.getElementById("promo-input").value;
+    if(promoInput === couponCode){
+        const total = document.getElementById("total-price");
+        let totalPrice = parseFloat(total.innerText);
+        const discount = (totalPrice*20)/100;
+        totalPrice = totalPrice - discount;
+
+        total.innerText = totalPrice;
+        
+    }
+    else{
+        
+    }
+    promoInput.value = ''; 
+});
